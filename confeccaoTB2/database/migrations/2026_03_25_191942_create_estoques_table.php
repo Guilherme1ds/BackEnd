db7278b56f08b7a43a098b6d22299ad81c980a4d
Guilherme1ds <bernardiguilherme24@gmail.com>
2026-03-25 16:39:43 -0300

    update 25/03

---
+++ confeccaoTB2/database/migrations/2026_03_25_191942_create_estoques_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estoques', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produto_id')->nullable()->constrained('produtos')->cascadeOnDelete();
+            $table->foreignId('insumo_id')->nullable()->constrained('insumos')->cascadeOnDelete();
+            $table->decimal('quantidade', 10, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estoques');
+    }
+};
